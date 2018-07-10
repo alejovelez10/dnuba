64d0a853d6c16ad289b21591c070c218de4a6369
alejovelez10 <alejovelez10@gmail.com>
2018-07-10 12:57:56 -0500

    agregando imagen

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411000157) do
+ActiveRecord::Schema.define(version: 20180710170438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cost_centers", force: :cascade do |t|
     t.string "name"
