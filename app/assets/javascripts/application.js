589779ac11bc50ad3b3a76d1ce0ff6a1a9aee9c3
alejovelez10 <alejovelez10@gmail.com>
2018-07-16 13:13:09 -0500

    organizando post

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,12 +30,4 @@ $('.ckeditor').ckeditor({
 
 
 
-CKEDITOR.config.toolbar= [
-       { name: 'list', items : [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
-        { name: 'insert', items: ['Image','Table','SpecialChar']},
-       { name: 'clipboard', items: ['Cut','Copy','Paste','PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-       { name: 'basicstyles', items: ['Bold','Italic','Strike', '-','RemoveFormat']}
-
-];
-
 });
