38595ee2a3bad3aea01e17fb3ebdcd9f367ebf2f
Tu Nombre <you@example.com>
2018-08-06 11:24:34 -0500

    cambios

---
+++ app/controllers/users/admin_controller.rb
@@ -0,0 +1,53 @@
+class Users::AdminController < Devise::RegistrationsController
+
+	def index
+		@user = User.all
+	end
+
+	def new_user
+		@user = User.new
+		
+	end
+
+	def crear_user
+		@user = User.create(email: params[:email], password: params[:password], password_confirmation:params[:password_confirmation])
+		if @user.save
+			redirect_to usuarios_path
+		end	
+		
+	end
+
+	def update_user
+      @user = User.find(params[:id])
+      if@user.update(email: params[:email], password: params[:password], password_confirmation:params[:password_confirmation])
+      redirect_to usuarios_path
+    else
+        render :edit  
+    end	
+
+	end
+
+	def editar_user
+		@user = User.find(params[:id])
+	end
+
+	def destroy_user
+		@user = User.find(params[:id])
+		if @user.destroy
+		 	redirect_to usuarios_path
+		end
+		
+			
+	end
+
+	private
+
+  	def set_user
+    	@user = User.find(params[:id]) 
+ 	end
+    
+
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,32 @@ Rails.application.routes.draw do
   resources :general_spends
   resources :cost_centers
   resources :type_spends
-  devise_for :users
+
+  devise_for :users, :controllers => { :registrations => "users/admin" }
+
+
+  devise_scope :user do
+    get "users/new_user", to: "users/admin#new_user", as: "new_user"
+    post "crear_user", to: "users/admin#crear_user", as: 'crear_user'
+    get "users/index", to: "users/admin#index", as: "usuarios"
+    get "users/editar_user/:id/", to: "users/admin#editar_user", as: "editar_user"
+    post "update_user", to: "users/admin#update_user", as: "update_user"
+    delete "destroy_user/:id", to: "users/admin#destroy_user", as: "destroy_user"
+
+
+  end
   
   root 'home#construccion'
-  
+
+  delete "destroy/:id", to:"contacts#destroy", as: "delete_contact"  
+
   get 'inicio', to:'home#inicio'
   get "contacts", to: "contacts#index"
 
-    post "cambios/:id", to: "posts#cambios", as: "cambios"
+  post "cambios/:id", to: "posts#cambios", as: "cambios"
   
   post "create_contacts", to: "contacts#create", as: "create_contacts"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+
 end
