4ff70944e2edda1a8270027fcced5846806f1cff
Tu Nombre <you@example.com>
2018-08-06 16:42:39 -0500

    menu2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,43 @@
 module ApplicationHelper
 
+	def show1(show)
 
-def show1(show)
+		puts show.name
+		puts "holaaaaaaaaaaaaaaaaaaaa"
+		
+	end
 
-	puts show.name
-	puts "holaaaaaaaaaaaaaaaaaaaa"
-	
-end
+	def admin_menu
+
+ 	@admin = ["", "", "", "", "", "", "", ""]
+
+ 	if controller_name == "cost_centers"
+ 		@admin = ["active", "", "", "", "", "", "", ""]
+
+	 	elsif controller_name == "type_spends"
+	 		@admin = ["", "active", "", "", "", "", "", ""]
+
+	 	elsif controller_name == "general_spends"
+	 		@admin = ["", "", "active", "", "", "", "", ""]
+
+	 	elsif controller_name == "incomes"
+	 		@admin = ["", "", "", "active", "", "", "", ""]
+
+	 	elsif controller_name == "contacts"
+	 		@admin = ["", "", "", "", "active", "", "", ""]
+
+	 	elsif controller_name == "admin"
+	 		@admin = ["", "", "", "", "", "active", "", ""]
+
+	 	elsif controller_name == "posts"
+	 		@admin = ["", "", "", "", "", "", "active", ""]
+
+ 	end
+
+
+ 	return @admin
+ 	
+ end
 
 
 end
