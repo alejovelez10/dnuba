9b9cc7f39b48efea99e5f0049534d2edc75ba842
alejovelez10 <alejovelez10@gmail.com>
2018-09-14 17:47:53 -0500

    hola

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711182427) do
+ActiveRecord::Schema.define(version: 20180913223511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bank_images", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "achivo"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
